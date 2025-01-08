91e9c9608ac9ebc9f547537b1fe5766ab5ff1bc0
MaxKEhiawey <83152189+MaxKEhiawey@users.noreply.github.com>
2025-01-08 17:51:04 +0000

    add singleNumberWithTriplicates

--- AgentApp/Algorithms/ContainsDuplicate .swift
+++ AgentApp/Algorithms/ContainsDuplicate .swift
@@ -86,3 +86,25 @@ func singleNumberWithTriplicates(_ nums: [Int]) -> Int {
     
     return ones
 }
+
+
+    class Solution {
+    func singleNumber(_ nums: [Int]) -> [Int] {
+         var xor = 0
+    for num in nums {
+        xor ^= num
+    }
+
+    let diff = xor & -xor
+    var num1 = 0, num2 = 0
+    for num in nums {
+        if num & diff == 0 {
+            num1 ^= num
+        } else {
+            num2 ^= num
+        }
+    }
+
+    return [num1, num2]
+    }
+}
